2c29fa942f23353c2e2501e3a57c403c073950c2
susztera <suszteragnes@hotmail.com>
2020-05-12 18:47:15 +0200

    temporary icon that better resembles product

--- unzipMasterGUId/unzipMaster.Designer.cs
+++ unzipMasterGUId/unzipMaster.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(unzipMaster));
             this.browseButton = new System.Windows.Forms.Button();
             this.txtPath = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -90,6 +91,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPath);
             this.Controls.Add(this.browseButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "unzipMaster";
             this.Text = "Unzip Master";
             this.Load += new System.EventHandler(this.Form1_Load);
